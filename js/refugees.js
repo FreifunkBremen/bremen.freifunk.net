029c84955920a2f91826226524732fa7c6323b48
Martin Geno <geno@fireorbit.de>
2015-09-27 17:10:40 +0200

    refugees.js

--- js/refugees.js
+++ js/refugees.js
@@ -1,30 +1,28 @@
 
-$(document).ready(function(){
-	var geoProzess = function(call){
-		$.ajax({
-			url:"/refugees.geojson",
-			beforeSend: function(xhr){
-				if (xhr.overrideMimeType)
-				{
-					xhr.overrideMimeType("application/json");
-				}
-			},
-			dataType: 'json',
-			contentType: 'application/json',
-			mimeType: "textPlain",
-			success: function(data){
-				var all = data.features.length;
-				var count = $.grep(data.features,function(i){
-						return (i.properties.Internet == "versorgt");
-				}).length;
-				prozet = Math.round(100*count/all);
-				if(call)
-					call(count,all);
-				else{
-					$("#geoprozess").width(prozet+'%');
-					$("#geoprozess").text('Es gibt noch <b>'+(all-count)+'</b> Flüchtlingsheime ohne Internet!');
-				}
+var geoProzess = function(call){
+	$.ajax({
+		url:"/refugees.geojson",
+		beforeSend: function(xhr){
+			if (xhr.overrideMimeType)
+			{
+				xhr.overrideMimeType("application/json");
 			}
-	});
-	}
+		},
+		dataType: 'json',
+		contentType: 'application/json',
+		mimeType: "textPlain",
+		success: function(data){
+			var all = data.features.length;
+			var count = $.grep(data.features,function(i){
+					return (i.properties.Internet == "versorgt");
+			}).length;
+			prozet = Math.round(100*count/all);
+			if(call)
+				call(count,all);
+			else{
+				$("#geoprozess").width(prozet+'%');
+				$("#geoprozess").text('Es gibt noch <b>'+(all-count)+'</b> Flüchtlingsheime ohne Internet!');
+			}
+		}
 });
+}
