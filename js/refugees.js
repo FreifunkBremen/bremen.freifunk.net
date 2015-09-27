d5a9b3a43de48f3922ddd6e8fd5162f40a90d113
Martin Geno <geno@fireorbit.de>
2015-09-27 16:55:03 +0200

    refugees.js

--- js/refugees.js
+++ js/refugees.js
@@ -1,19 +1,29 @@
 
 $(document).ready(function(){
 	var geoProzess = function(call){
-		$.getJSON("http://bremen.freifunk.net/refugees.geojson",{},function( data ) {
-			var all = data.features.length;
-			var count = $.grep(data.features,function(i){
-					return (i.properties.Status == "versorgt");
-			}).length;
-			prozet = Math.round(100*count/all);
-			if(call)
-				call(count,all);
-			else{
-				$("#geoprozess").width(prozet+'%');
-				$("#geoprozess").text(prozet+'% Complete');
+		$.ajax({
+			url:"http://bremen.freifunk.net/refugees.geojson",
+			beforeSend: function(xhr){
+				if (xhr.overrideMimeType)
+				{
+					xhr.overrideMimeType("application/json");
+				}
+			},
+			contentType: 'application/json',
+			success: function(data){
+				var all = data.features.length;
+				var count = $.grep(data.features,function(i){
+						return (i.properties.Status == "versorgt");
+				}).length;
+				prozet = Math.round(100*count/all);
+				if(call)
+					call(count,all);
+				else{
+					$("#geoprozess").width(prozet+'%');
+					$("#geoprozess").text(prozet+'% Complete');
+				}
 			}
-	} );
+	});
 	}
 	geoProzess();
 });
