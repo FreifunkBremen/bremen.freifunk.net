6389c1842c46f3b64884547ff0865fcb4a133d18
Martin Geno <geno@fireorbit.de>
2015-11-03 19:12:09 +0100

    refugees.js add Anzahl

--- js/refugees.js
+++ js/refugees.js
@@ -14,7 +14,7 @@ var geoProzess = function(callback){
 		success: function(data){
 			var remaining = 0;
 			for(var i=0;i<data.features.length;i++){
-				var item = ata.features[i];
+				var item = data.features[i];
 				if(item.Anzahl){
 					remaining += item.Anzahl;
 				}else{
