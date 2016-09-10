8e23c3b81a770d1c69edcaa4637cec49603ad534
Jan-Philipp Litza <janphilipp@litza.de>
2016-09-10 15:00:47 +0200

    Implement navigation hiding/showing

--- assets/main.js
+++ assets/main.js
@@ -14,4 +14,48 @@ document.addEventListener("DOMContentLoaded", function() {
         document.documentElement.classList.toggle("navopen");
       });
   });
+
+  // throttle the "scroll" event
+  // from https://developer.mozilla.org/en-US/docs/Web/Events/scroll
+  var throttle = function(type, name, obj) {
+    var obj = obj || window;
+    var running = false;
+    var func = function() {
+      if (running) { return; }
+      running = true;
+      requestAnimationFrame(function() {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+  throttle("scroll", "throttledScroll");
+
+  var last_scrollY = window.scrollY;
+  // shrink the header on scrolling
+  window.addEventListener("throttledScroll", function(ev) {
+    var header = document.querySelector('header');
+    var threshold_down = Math.max(
+      header.clientHeight - document.querySelector('#mainnav li.active ul').offsetHeight,
+      1
+    );
+    var threshold_up = Math.max(
+      threshold_down - document.getElementById('mainnav').offsetHeight,
+      1
+    );
+
+    if (window.scrollY >= threshold_down)
+      header.classList.add('fixed');
+    else if (window.scrollY < threshold_up)
+      header.classList.remove('fixed');
+
+    if (window.scrollY < last_scrollY || window.scrollY < threshold_up)
+      header.classList.add('detail');
+    else if (window.scrollY > last_scrollY)
+      header.classList.remove('detail');
+
+    last_scrollY = window.scrollY;
+  });
+  window.dispatchEvent(new CustomEvent("throttledScroll"));
 });
