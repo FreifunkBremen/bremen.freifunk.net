46e43922c3997f696d2b5d49c8cbcba0acb154c2
Oliver Gerlich <oliver.gerlich@gmx.de>
2026-05-18 12:23:31 +0200

    adjust Grafana query to new Grafana version
    The previous query did not work any more (probably due to yesterday's
    upgrade from Grafana 12 to 13).

--- assets/index.js
+++ assets/index.js
@@ -190,7 +190,7 @@ document.addEventListener("DOMContentLoaded", function() {
   (function() {
     var xhr = window.XMLHttpRequest? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'),
       query = 'SELECT last("clients.total") AS "clients", last("nodes") AS "nodes" FROM "global"';
-    xhr.open('GET', 'https://grafana.bremen.freifunk.net/api/datasources/proxy/1/query?db=ffhb-nodes&q=' + encodeURIComponent(query) + '&u=public&p=public');
+    xhr.open('GET', 'https://grafana.bremen.freifunk.net/api/datasources/proxy/uid/000000001/query?db=ffhb-nodes&q=' + encodeURIComponent(query) + '&u=public&p=public');
     xhr.onreadystatechange = function() {
       if (xhr.readyState <= 3 || xhr.status != 200)
         return;
